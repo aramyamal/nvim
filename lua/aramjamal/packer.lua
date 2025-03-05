d14aee3b56fa0fc00bfed85489a2f015d900b204
aramyamal <aram@aramjamal.com>
2025-03-05 14:55:10 +0100

    fix: bootstrap packer on repo clone

--- lua/aramjamal/packer.lua
+++ lua/aramjamal/packer.lua
@@ -128,4 +128,10 @@ return require('packer').startup(function(use)
     use 'nvim-tree/nvim-web-devicons'
 
     use "3rd/image.nvim"
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
