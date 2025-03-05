42297d2474058eb61db8007d6459a4274f61e0fc
aramyamal <aram@aramjamal.com>
2025-03-05 15:36:56 +0100

    fix: load packer before plugin configs for bootstrap

--- lua/aramjamal/init.lua
+++ lua/aramjamal/init.lua
@@ -1,5 +1,6 @@
 require("aramjamal.remap")
 require("aramjamal.set")
+require("aramjamal.packer")
 -- Automatically adjust theme of terminal and nvim according to system theme
 if vim.fn.system('gsettings get org.gnome.desktop.interface color-scheme'):gsub('\n', '') == "'default'" then
     vim.o.background = "light"
